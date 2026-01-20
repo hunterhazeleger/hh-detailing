65524f95d090cfb6dad8d3b6e2c11e8909c6c11f
hunterhazeleger <hunterhazeleger@gmail.com>
2026-01-20 15:01:05 +0100

    Move init_db call to the end of app.py
    Call init_db function at the end of the script instead of at the start.

--- app.py
+++ app.py
@@ -249,6 +249,7 @@ def admin_delete_booking():
     return redirect(url_for("admin"))
 
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", "5000")), debug=True)
